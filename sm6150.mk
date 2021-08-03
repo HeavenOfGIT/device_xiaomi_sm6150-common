84eecd7f4fcc5b688a4070015c1da1895f0a8641
LCarlosp <laerciocp2830@gmail.com>
2021-08-03 09:51:27 -0300

    add-CameraGo

--- sm6150.mk
+++ sm6150.mk
@@ -93,7 +93,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor
 
 PRODUCT_PACKAGES += \
-    GCamGOPrebuilt
+    CameraGo
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
